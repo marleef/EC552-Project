18a7d7206be4e46dc9ab9ee558d7ba743c678716
Marlee Feltham <55824801+marleef@users.noreply.github.com>
2022-04-20 19:05:51 -0400

    added combineGraphs and minCost fcns

--- project.py
+++ project.py
@@ -7,6 +7,8 @@ EC552 Spring 22 Final Project
 import csv
 from logging import StringTemplateStyle
 import os
+import copy
+import random
 
 
 #################### DATA PARSE #####################
@@ -15,23 +17,25 @@ def createAsmGraph(part, hashMem):
     # (1) memoization case
     if part in hashMem.values():
         return hashMem.values(part)
-    if (len(part) == 1): 
-        return part #return new graph with given part
+    if (len(part) == 1):
+        return part  # return new graph with given part
 
     # graph framework:
     graph = {'stages': '', 'steps': '', 'sharing': ''}
 
-    for i in range(len(part)): 
-        subpartL = part.split(0,i)
+    for i in range(len(part)):
+        subpartL = part.split(0, i)
         subpartR = part.split(i, len(part))
-        graphL = createAsmGraph(subpartL, hashMem) #recursive, calls subpart as new part
-        graphR = createAsmGraph(subpartR, hashMem) #recursive, calls subpart as new part 
-        
-        graphNew = combineGraphs(graphL, graphR) #combine L/R into new graph 
+        # recursive, calls subpart as new part
+        graphL = createAsmGraph(subpartL, hashMem)
+        # recursive, calls subpart as new part
+        graphR = createAsmGraph(subpartR, hashMem)
+
+        graphNew = combineGraphs(graphL, graphR)  # combine L/R into new graph
         graphBest = minCost(graphNew, graphBest)
 
-    hashMem.insert(part, graphBest) #part is key, graphBest is value
-    return graphBest   
+    hashMem.insert(part, graphBest)  # part is key, graphBest is value
+    return graphBest
 
     # (2) base case
     # if (len(part)) == 1:
@@ -50,24 +54,39 @@ def createAsmGraph(part, hashMem):
     # (4) add best graph to hash table and return
 
 # combine graphs function
+
+
 def combineGraphs(graphL, graphR):
     """USING DUMMY OUTPUTS FOR NOW"""
-    graphNew = graphL 
-    return graphNew 
-    # (1) return graph created from combining two child graphs
+    # Return graph created from combining two child graphs
+    graphNew = copy.deepcopy(graphL)  # make a copy of graphL
+    graphNew.update(graphR)  # add graphR to graphNew (merges)
 
-    # (2) calculate cost of new graph
+    # Calculate the cost of new graph
+    graphNew['stages'] = max(graphL['stages'], graphR['stages']) + 1
+    graphNew['steps'] = graphL['steps'] + graphR['steps'] + 1
 
+    return graphNew
 
 # min cost graph function
+
+
 def minCost(graph0, graph1):
-     """USING DUMMY OUTPUT FOR NOW"""
-     return graph0
+    """USING DUMMY OUTPUT FOR NOW"""
+    if graph0['stages'] < graph1['stages']:
+        return graph0
+    if graph1['stages'] < graph0['stages']:
+        return graph1
 
-    # (1) num of stages always take priority -> change ?
-    # (2) if num of stages = then graph w/ less steps is lower cost
-    # (3) graphs have identical cost so arbitrarily choose one
+    # If number of stages equal, then graph with less steps is lower cost
+    if graph0['steps'] < graph1['steps']:
+        return graph0
+    if graph1['steps'] < graph0['steps']:
+        return graph1
 
+    # Graphs have identical cost so arbitrarily choose one
+    g = [graph0, graph1]
+    return random.choice(g)
 
 #################### MAIN ###########################
 
@@ -80,13 +99,13 @@ def main():
     for rows in file:
         # print(rows)
         fileList.append(rows)
-    #print(fileList)
+    # print(fileList)
 
     hashMem = {}  # empty hashMem dictionary
     part = 'a'
 
     graphOut = createAsmGraph(part, hashMem)
-    print(graphOut) 
+    print(graphOut)
 
 
 if __name__ == '__main__':
